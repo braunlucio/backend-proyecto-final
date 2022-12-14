65cb252832bac79c195c2d33a548fc6345f27e1c
lucio <braunlucio1@gmail.com>
2022-12-14 00:42:21 -0300

    vuelta al original

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Book = require("../model/Book");
 const booksController = require("../controllers/books-controller");
 
-router.get("/" + booksController.getAllBooks);
+router.get("/", booksController.getAllBooks);
 router.post("/", booksController.addBook);
 router.get("/:id", booksController.getById);
 router.put("/:id", booksController.updateBook);
